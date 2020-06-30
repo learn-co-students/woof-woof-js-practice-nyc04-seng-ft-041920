ee0904fe21fdefb58823ec8bc02a596f6f553968
jinnic <yoohyojin@gmail.com>
2020-06-29 22:23:43 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
-fetch('http://localhost:3000/pups')
-  .then(res => res.json())
-  .then(renderDogs)
+getDogs().then(renderDogs)
+
+function getDogs(){
+  return fetch('http://localhost:3000/pups')
+    .then(res => res.json())
+}
+
+
 
 function renderDogs(dogs){
+  const dogBar = document.querySelector('#dog-bar')
+  dogBar.innerHTML = ""
+
+  console.log(dogs)
   dogs.forEach(dog => {
     const dogSpan = document.createElement('span')
     dogSpan.innerText = dog.name
     dogSpan.setAttribute('data-id', dog.id)
     dogSpan.addEventListener('click', e=>{
+      // debugger
       console.log(e.target.dataset.id)
       getDogInfo(e.target.dataset.id)
     })
-
-    const dogBar = document.querySelector('#dog-bar')
     dogBar.append(dogSpan)
+    debugger
   });
 }
 
@@ -26,7 +35,7 @@ function getDogInfo(dogId){
 
 function renderDog(dogObj){
   const dogInfo = document.querySelector('#dog-info')
-  const dogStatus = dogObj.isGoodDog ? "Bad Dog!" : "Good Dog!"
+  const dogStatus = dogObj.isGoodDog ? "Good Dog!" : "Bad Dog!"
   dogInfo.innerHTML = `
     <img src=${dogObj.image}>
     <h2>${dogObj.name}</h2>
@@ -36,11 +45,13 @@ function renderDog(dogObj){
   dogInfo.querySelector('button').addEventListener('click', e =>{
     // debugger
     const dogId = dogObj.id
-    const dogIsGood = e.target.innerText === "Good Dog!" ? true : false
+    const dogIsGood = e.target.innerText === "Good Dog!" ? false : true
+    console.log(dogIsGood)
     const dogUpdateObj = {
       'isGoodDog': dogIsGood
     }
     updateDogInfo(dogUpdateObj,dogId)
+    
   })
 }
 
@@ -56,3 +67,19 @@ function updateDogInfo(dogUpdateObj, dogId){
   .then(res => res.json())
   .then(renderDog)
 }
+
+const filterBtn = document.querySelector('#good-dog-filter')
+filterBtn.addEventListener('click', e => {
+  if(e.target.innerText === "Filter good dogs: ON") {
+     e.target.innerText = "Filter good dogs: OFF" 
+     getDogs().then(renderDogs)
+   }else{
+      e.target.innerText = "Filter good dogs: ON" 
+      getDogs().then(dogs => {
+        const filterDogs = dogs.filter(dog => dog.isGoodDog === true)
+        renderDogs(filterDogs)
+      })
+   }
+
+})
+
