f38c4ad004b78283f41d8540d3e2be1566064485
github username <github email address>
2020-06-29 22:02:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(){
   const dogBar = document.querySelector("#dog-bar")
   const dogInfo = document.querySelector("#dog-info")
+  const goodDogFilter = document.querySelector("#good-dog-filter")
+
   console.log(dogInfo)
-  fetch('http://localhost:3000/pups')
-  .then(response => response.json())
-  .then(dogData => {
-    for(const dog of dogData){
-      console.log(dog)
-      const dogSpan = document.createElement("span")
-      dogSpan.dataset.id = dog.id
-      dogSpan.textContent = dog.name
-      dogBar.append(dogSpan)
-    }
+  // fetch('http://localhost:3000/pups')
+  // .then(response => response.json())
+  // .then(dogData => {
+  //   for(const dog of dogData){
+  //     console.log(dog)
+  //     const dogSpan = document.createElement("span")
+  //     dogSpan.dataset.id = dog.id
+  //     dogSpan.textContent = dog.name
+  //     dogBar.append(dogSpan)
+  //   }
 
-    dogBar.addEventListener("click", function(event){
-      console.log(event.target)
-      if (event.target.matches("span")){
-        const clickedDog = dogData[event.target.dataset.id -1]
-        dogInfo.innerHTML = `<img src="${clickedDog.image}"><h2>${clickedDog.name}</h2>
-         <button class="dog-button" data-id="${clickedDog.id}">${clickedDog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>`
+  //   dogBar.addEventListener("click", function(event){
+  //     console.log(event.target)
+  //     if (event.target.matches("span")){
+  //       const clickedDog = dogData[event.target.dataset.id -1]
+  //       dogInfo.innerHTML = `<img src="${clickedDog.image}"><h2>${clickedDog.name}</h2>
+  //        <button class="dog-button" data-id="${clickedDog.id}">${clickedDog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>`
 
-        const dogButton = document.querySelector(".dog-button")
-        dogButton.addEventListener("click", onGoodDogButtonClick)
-      }
-    })
+  //       const dogButton = document.querySelector(".dog-button")
+  //       dogButton.addEventListener("click", onGoodDogButtonClick)
+  //     }
+  //   })
     
-  });
+  // });
+  showAllDogs()
 
   // outside fetch
   function onGoodDogButtonClick(){
@@ -60,6 +63,88 @@ document.addEventListener("DOMContentLoaded", function(){
   console.error('Error:', error);
 });
   }
+
+
+//bonus filter good dogs
+console.log(goodDogFilter)
+goodDogFilter.addEventListener("click", function(e){
+  console.log(goodDogFilter.textContent.includes("OFF"))
+  if (goodDogFilter.textContent.includes("OFF")){
+    goodDogFilter.textContent = "Filter good dogs: ON"
+    //show only good dogs
+    showGoodDogs()
+  }
+  else{
+    goodDogFilter.textContent = "Filter good dogs: OFF"
+    //show all dogs
+
+    showAllDogs()
+  }
+})
+
+
+// get and display all dogs
+function showAllDogs(){
+  fetch('http://localhost:3000/pups')
+  .then(response => response.json())
+  .then(dogData => {
+    dogBar.innerHTML = ""
+
+    for(const dog of dogData){
+      console.log(dog)
+      const dogSpan = document.createElement("span")
+      dogSpan.dataset.id = dog.id
+      dogSpan.textContent = dog.name
+      dogBar.append(dogSpan)
+    }
+
+    dogBar.addEventListener("click", function(event){
+      console.log(event.target)
+      if (event.target.matches("span")){
+        const clickedDog = dogData[event.target.dataset.id -1]
+        dogInfo.innerHTML = `<img src="${clickedDog.image}"><h2>${clickedDog.name}</h2>
+         <button class="dog-button" data-id="${clickedDog.id}">${clickedDog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>`
+
+        const dogButton = document.querySelector(".dog-button")
+        dogButton.addEventListener("click", onGoodDogButtonClick)
+      }
+    })
+    
+  })
+}
+
+
+function showGoodDogs(){
+  fetch('http://localhost:3000/pups')
+  .then(response => response.json())
+  .then(dogData => {
+    dogBar.innerHTML = ""
+
+    for(const dog of dogData){
+      if (dog.isGoodDog){
+
+      console.log(dog)
+      const dogSpan = document.createElement("span")
+      dogSpan.dataset.id = dog.id
+      dogSpan.textContent = dog.name
+      dogBar.append(dogSpan)
+    }
+  }
+
+    dogBar.addEventListener("click", function(event){
+      console.log(event.target)
+      if (event.target.matches("span")){
+        const clickedDog = dogData[event.target.dataset.id -1]
+        dogInfo.innerHTML = `<img src="${clickedDog.image}"><h2>${clickedDog.name}</h2>
+         <button class="dog-button" data-id="${clickedDog.id}">${clickedDog.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button>`
+
+        const dogButton = document.querySelector(".dog-button")
+        dogButton.addEventListener("click", onGoodDogButtonClick)
+      }
+    })
+    
+  })
+}
 
 
 })
