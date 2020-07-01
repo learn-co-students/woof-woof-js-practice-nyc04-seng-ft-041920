30d23648d870d81734a661b94c12507e1960652f
residenttrickster <metkusblackus@gmail.com>
2020-06-30 20:13:22 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,106 +1,62 @@
-let pups = []
-let filterOn = false
+//DOM elements
+const dogsContainer = document.querySelector("#dog-bar");
+const singleDog = document.querySelector("#dog-info");
+const button = singleDog.querySelector("button")
 
-// when (the filter button is click)
-const filterButton = document.querySelector("#good-dog-filter")
-filterButton.addEventListener("click", () => {
-// slap (the updated pups list) on the DOM
-// console.log(pups)
-filterOn = !filterOn
-if (filterOn) {
-filterButton.innerText = "Filter good dogs: On"
-} else {
-filterButton.innerText = "Filter good dogs: OFF"
-}
-// need the array of pups
-// once we have the array we need to filter it: 
-const filteredPups = pups.filter(pup => {
-// based on isGoodDog attribute, compare it to the filterOn variable
-if (filterOn) {
-    return pup.isGoodDog
-} else {
-    return true
-}
-})
-console.log(filteredPups)
-// once we have our filtered list, slap it on the the DOM
-let pupsList = document.querySelector("#dog-bar")
-pupsList.innerHTML = ""
-renderPups(filteredPups)
-})
+//Event listeners
 
+//Render helpers 
 
-//  First Thing to do (a get for all pups) fetch
-fetch("http://localhost:3000/pups")
-.then(r => r.json())
-.then(pupsArray => {
-pups = pupsArray
-// gently tap (each pup) on the DOM
-renderPups(pupsArray)
-})
+function renderOneDog(dogObject) {
+    singleDog.innerHTML = `<img src="${dogObject.image}" alt="${dogObject.name}">
+    <h2>${dogObject.name} </h2> 
+    <button>${dogObject.name ? "Good Dog!" : "Bad Dog!"}</button>`
 
-function renderPups(pupsArray) {
-// find out where you want to add the pups to (use querySelector)
-let pupsList = document.querySelector("#dog-bar")
-
-// create an element for each pup (<span>Mr. Bonkers</span>)
-pupsArray.forEach(pup => {
-let pupsSpan = document.createElement("span")
-pupsSpan.innerText = pup.name
-// append the element to our container
-pupsList.append(pupsSpan)
-
-// when (a click on a dog span) event happens
-pupsSpan.addEventListener('click', () => {
-
-    // slap (the dog details) on the DOM
-    // grab the container that will show the dog details
-    let dogInfo = document.querySelector("#dog-info")
-
-    // add this HTML to the container:
-    {/* <img src=dog_image_url>
-    <h2>Mr. Bonkers</h2>
-    <button>Good Dog!</button> */}
-    dogInfo.innerHTML = `
-    <img src=${pup.image}>
-    <h2>${pup.name}</h2>
-    <button>${pup.isGoodDog} Dog!</button>
-    `
-    const button = dogInfo.querySelector("button")
-
-    if (pup.isGoodDog === true) {
-    button.innerText = "Good Dog!"
+    const button = singleDog.querySelector("button")
+    if (dogObject.isGoodDog === true) {
+        button.textContent = "Good Dog!"
     } else {
-    button.innerText = "Bad Dog!"
+        button.textContent = "Bad Dog!"
     }
 
-    // when (a click on the dog button) event happens
-    button.addEventListener("click", () => {
-    pup.isGoodDog = !pup.isGoodDog
-    const updatedDogInfo = { isGoodDog: pup.isGoodDog }
+    button.addEventListener("click", function(event){
+        dogObject.isGoodDog = !dogObject.isGoodDog;
 
-    // do (a patch) fetch
-    fetch(`http://localhost:3000/pups/${pup.id}`, {
-        method: "PATCH",
-        headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json",
-        },
-        body: JSON.stringify(updatedDogInfo)
-    })
-        .then(r => r.json())
-        .then(newPupInfo => {
-        // slap (the updated dog info) on the DOM
-        if (newPupInfo.isGoodDog === true) {
-            button.innerText = "Good Dog!"
-        } else {
-            button.innerText = "Bad Dog!"
-        }
-        })
-
-    })
-
-
-})
+        fetch(`http://localhost:3000/pups/${dogObject.id}`, {
+                method: 'PATCH', 
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(dogObject)
+            })
+                .then(r => r.json())
+                .then(dogUpdate => {
+                    if (dogUpdate.isGoodDog === true) {
+                        button.textContent = "Good Dog!"
+                    } else {
+                        button.textContent = "Bad Dog!"
+                    }
+                })
 })
 }
+
+
+function renderAllDogs(dogObject) {
+    const dogSpan = document.createElement("span")
+    dogSpan.dataset.id = dogObject.id
+    dogSpan.textContent = dogObject.name
+
+    dogsContainer.append(dogSpan)
+
+    dogSpan.addEventListener("click", function(event) {
+        renderOneDog(dogObject)
+    })
+}
+
+//Initial render
+
+fetch('http://localhost:3000/pups')
+.then(response => response.json())
+.then(dogArray => {
+    dogArray.forEach(renderAllDogs)
+});
